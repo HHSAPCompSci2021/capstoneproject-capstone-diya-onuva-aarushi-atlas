b83a9cca4a9f8e286209d7e4136b4266c5fb97f8
Onuva Ekram <oekram574@student.fuhsd.org>
2022-05-10 12:35:34 -0700

    Update DrawingSurface.java

--- CapstoneProject/src/DrawingSurface.java
+++ CapstoneProject/src/DrawingSurface.java
@@ -4,12 +4,20 @@ public class DrawingSurface extends PApplet {
 	
 	Introduction intro;
 	Menu menu;
+	boolean showIntro;
 	
 	public DrawingSurface() {
-		
+		showIntro = true;
+		intro = new Introduction("src/fileData/IntroTree.jpeg");
+		menu = new Menu("src/fileData/MenuMap.jpeg");
 	}
 	
 	public void draw() {
-		
+		if (showIntro) intro.draw(this);
+		else menu.draw(this);
+	}
+	
+	public void switchToMenu() {
+		showIntro = false;
 	}
 }
