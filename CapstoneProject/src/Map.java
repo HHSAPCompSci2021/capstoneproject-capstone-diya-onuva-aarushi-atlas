edfb724ae388ef9974d5a226d6e380ad9bbda51b
Onuva Ekram <oekram574@student.fuhsd.org>
2022-05-11 12:38:59 -0700

    Update Map.java

--- CapstoneProject/src/Map.java
+++ CapstoneProject/src/Map.java
@@ -10,8 +10,18 @@ public class Map {
 		
 	}
 	
-	public String getCountry(double x, double y)
+	public String getCountry(int r, int g, int b)
 	{
+		
+		if (r == 50 && g == 65 && b == 203) return "Morocco";
+		else if (r == 112 && g == 143 && b == 215) return "Algeria";
+//		int r = PApplet.round(draw.red(draw.color(draw.get(draw.mouseX, draw.mouseY))));
+//		int g = PApplet.round(draw.green(draw.color(draw.get(draw.mouseX, draw.mouseY))));
+//		int b = PApplet.round(draw.blue(draw.color(draw.get(draw.mouseX, draw.mouseY))));
+//		
+//		Map m = new Map();
+//		System.out.println(m.getCountry(r,  g,  b));
+//		
 		return null;
 		
 	}
