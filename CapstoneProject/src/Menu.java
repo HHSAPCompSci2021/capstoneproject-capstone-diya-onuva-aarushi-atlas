4a179b217b0f9f51a160155bb264104984a66369
Onuva Ekram <oekram574@student.fuhsd.org>
2022-05-13 15:17:05 -0700

    Update Menu.java

--- CapstoneProject/src/Menu.java
+++ CapstoneProject/src/Menu.java
@@ -5,7 +5,8 @@ import processing.core.PConstants;
 import processing.core.PFont;
 
 
-public class Menu {
+public class Menu extends Screen {
+
 	private StudyMode study;
 	private QuizMode quiz;
 	private DrawingMode drawing;
@@ -39,16 +40,91 @@ public class Menu {
 	public void draw(PApplet draw) 
 	{
 		
+		if (activeMode == 0) menuDraw(draw);
+		else if (activeMode == 1) study.draw(draw);
+		else if (activeMode == 2) quiz.draw(draw);
+		else if (activeMode == 3) drawing.draw();
+		else inst.draw(draw);
+		
+	}
+	
+	public void menuDraw(PApplet draw) {
+		if(draw.mousePressed && draw.mouseX <= (draw.width/2 + 175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 - 75) && (draw.height/2 - 125) <= draw.mouseY)
+		{
+			switchActiveScreen(1);
+			pressed = true;
+			//draw.fill(0, 185, 35);
+		}
+				
+		//click on Quiz
+		if(draw.mousePressed && draw.mouseX <= (draw.width/2 + 175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 + 25) && (draw.height/2 - 25) <= draw.mouseY)
+		{
+			switchActiveScreen(2);
+			pressed = true;
+			//draw.fill(0, 185, 35);
+		}
+				
+		//click on Draw
+		if(draw.mousePressed && draw.mouseX <= (draw.width/2 +175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 + 125) && (draw.height/2 + 75) <= draw.mouseY)
+		{
+			switchActiveScreen(3);
+			pressed = true;
+			//draw.fill(0, 185, 35);
+		}
+		
+		//click on Instructions
+		if(draw.mousePressed && draw.mouseX <= 75 && 25 <= draw.mouseX && draw.mouseY <= 75 && 25 <= draw.mouseY)
+		{
+			switchActiveScreen(3);
+			pressed = true;
+			
+		}
+		
 		draw.background(255,255,255);
 		
 		draw.fill(130, 185, 35);
 		
-		draw.rect(draw.width/2, draw.height/2 - 100, 350, 50); //Study
-		draw.rect(draw.width/2, draw.height/2, 350, 50); //Quiz
-		draw.rect(draw.width/2, draw.height/2 + 100, 350, 50); //Draw
+		if (pressed) {
+			if (activeMode == 1) {
+				draw.fill(0, 185, 35);
+				draw.rect(draw.width/2, draw.height/2 - 100, 350, 50);
+				draw.fill(130, 185, 35);
+				draw.rect(draw.width/2, draw.height/2, 350, 50); //Quiz
+				draw.rect(draw.width/2, draw.height/2 + 100, 350, 50); //Draw
+				draw.rect(50, 50, 50, 50);
+			}
+			else if (activeMode == 2) {
+				draw.fill(0, 185, 35);
+				draw.rect(draw.width/2, draw.height/2, 350, 50); //Quiz
+				draw.fill(130, 185, 35);
+				draw.rect(draw.width/2, draw.height/2 - 100, 350, 50);
+				draw.rect(draw.width/2, draw.height/2 + 100, 350, 50); //Draw
+				draw.rect(50, 50, 50, 50);
+			}
+			else if (activeMode == 3) {
+				draw.fill(0, 185, 35);
+				draw.rect(draw.width/2, draw.height/2 + 100, 350, 50); //Draw
+				draw.fill(130, 185, 35);
+				draw.rect(draw.width/2, draw.height/2 - 100, 350, 50);
+				draw.rect(draw.width/2, draw.height/2, 350, 50); //Quiz
+				draw.rect(50, 50, 50, 50);
+			}
+			else {
+				draw.fill(0, 185, 35);
+				draw.rect(50, 50, 50, 50);
+				draw.fill(130, 185, 35);
+				draw.rect(draw.width/2, draw.height/2 - 100, 350, 50);
+				draw.rect(draw.width/2, draw.height/2, 350, 50); //Quiz
+				draw.rect(draw.width/2, draw.height/2 + 100, 350, 50); //Draw
+			}
+		}
+		else {
+			draw.rect(50, 50, 50, 50);
+			draw.rect(draw.width/2, draw.height/2 - 100, 350, 50); //Study
+			draw.rect(draw.width/2, draw.height/2, 350, 50); //Quiz
+			draw.rect(draw.width/2, draw.height/2 + 100, 350, 50); //Draw
+		}
 		
-		draw.fill(0, 185, 35);
-		draw.rect(50, 50, 50, 50); //Instructions
 		draw.fill(0);
 		draw.textSize(40);
 		draw.text("?", 50, 60);
@@ -65,81 +141,7 @@ public class Menu {
 		draw.text("Quiz Mode", draw.width/2, draw.height/2 + 10);
 		draw.text("Draw Mode", draw.width/2, draw.height/2 + 110);
 		
-		mode = 0;
-		
-		//click on Study
-		if(draw.mousePressed && draw.mouseX <= (draw.width/2 + 175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 - 75) && (draw.height/2 - 125) <= draw.mouseY)
-		{
-			Menu.switchActiveScreen(1);
-			activeMode = 1;
-			mode = 1;
-			pressed = true;
-			draw.fill(0, 185, 35);
-			draw.rect(draw.width/2, draw.height/2 - 100, 350, 50);
-//			draw.background(255);
-//			study.draw(draw);
-//			switch screen implementation
-			Menu.switchActiveScreen(1);
-			study.draw(draw);
-//			surface.switchScreen(ScreenSwitcher.STUDY_SCREEN);
-		}
-				
-		//click on Quiz
-		if(draw.mousePressed && draw.mouseX <= (draw.width/2 + 175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 + 25) && (draw.height/2 - 25) <= draw.mouseY)
-		{
-			mode = 2;
-			pressed = true;
-			draw.fill(0, 185, 35);
-			draw.rect(draw.width/2, draw.height/2, 350, 50);
-
-			Menu.switchActiveScreen(2);
-			quiz.draw(draw);
-//			draw.background(255);
-//			quiz.draw(draw);
-//			switch screen implementation
-//			surface.switchScreen(ScreenSwitcher.QUIZ_SCREEN);
-			}
-				
-		//click on Draw
-		if(draw.mousePressed && draw.mouseX <= (draw.width/2 +175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 + 125) && (draw.height/2 + 75) <= draw.mouseY)
-		{
-			mode = 3;
-			pressed = true;
-			draw.fill(0, 185, 35);
-			draw.rect(draw.width/2, draw.height/2 + 100, 350, 50);
-
-			Menu.switchActiveScreen(3);
-			drawing.draw();
-//			surface.switchScreen(ScreenSwitcher.DRAW_SCREEN);
-//			draw.background(255);
-//			drawing.draw();
-//			switch screen implementation
-		}
-		
-		
-		
-		
-		//click on Instructions
-		if(draw.mousePressed && draw.mouseX <= 75 && 25 <= draw.mouseX && draw.mouseY <= 75 && 25 <= draw.mouseY)
-		{
-			mode = 4;
-			pressed = true;
-			inst.draw(draw);
-		}
-		
-//		if(activeMode == 1) {
-//			study.draw(draw);
-//		}
-//		if(activeMode == 2) {
-//			quiz.draw(draw);
-//		}
-//		if(activeMode == 3) {
-//			drawing.draw();
-//		}
-//		if(activeMode == 4) {
-//			inst.draw(draw);
-//		}
-		
+		pressed = false;
 	}
 	
 	public int getHighScore()
@@ -163,37 +165,5 @@ public class Menu {
 			return "No mode selected";
 	}
 	
-	public void mousePressed(PApplet draw)
-	{
-		
-		//click on Study
-		if(draw.mousePressed && draw.mouseX <= (draw.width/2 + 175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 - 75) && (draw.height/2 - 125) <= draw.mouseY)
-		{
-			mode = 1;
-			pressed = true;
-			draw.fill(0, 185, 35);
-			draw.rect(draw.width/2, draw.height/2 - 100, 350, 50);
-//			switch screen implementation
-		}
-				
-		//click on Quiz
-		if(draw.mousePressed && draw.mouseX <= (draw.width/2 + 175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 + 25) && (draw.height/2 - 25) <= draw.mouseY)
-		{
-			mode = 2;
-			pressed = true;
-			draw.fill(0, 185, 35);
-			draw.rect(draw.width/2, draw.height/2, 350, 50);
-//			switch screen implementation
-			}
-				
-		//click on Draw
-		if(draw.mousePressed && draw.mouseX <= (draw.width/2 +175) && (draw.width/2 - 175) <= draw.mouseX && draw.mouseY <= (draw.height/2 + 125) && (draw.height/2 + 75) <= draw.mouseY)
-		{
-			mode = 3;
-			pressed = true;
-			draw.fill(0, 185, 35);
-			draw.rect(draw.width/2, draw.height/2 + 100, 350, 50);
-//			switch screen implementation
-		}
-	}
+	
 }
