00d2c99136bdc2cd43f3f81c2e7da4cd0a44f3e8
Diya Bengani <dbengani198@student.fuhsd.org>
2022-05-13 12:51:12 -0700

    updates on quiz mode

--- CapstoneProject/src/QuizMode.java
+++ CapstoneProject/src/QuizMode.java
@@ -2,15 +2,68 @@ import java.util.ArrayList;
 
 import processing.core.PApplet;
 
-public class QuizMode extends Map{
+public class QuizMode extends Screen{
 
 	int score;
 	ArrayList<String> unlocked;
+	ArrayList<String> countries;
 	private DrawingSurface surface;
 	
 	public QuizMode ()
 	{
+		countries = new ArrayList<String>();
 		
+		countries.add("The Gambia");
+		countries.add("Liberia");
+		countries.add("Benin");
+		countries.add("Egypt");
+		countries.add("Zimbabwe");
+		countries.add("Ghana");
+		countries.add("Gabon");
+		countries.add("South Africa");
+		countries.add("Eritrea");
+		countries.add("Sengal");
+		countries.add("Burkina Faso");
+		countries.add("Somalia");
+		countries.add("Mozambique");
+		countries.add("The Comoros");
+		countries.add("Madagascar");
+		countries.add("Angola");
+		countries.add("Uganda");
+		countries.add("Morocco");
+		countries.add("Western Sahara");
+		countries.add("Algeria");
+		countries.add("Libya");
+		countries.add("Sudan");
+		countries.add("Tunisia");
+		countries.add("Ethiopia");
+		countries.add("Kenya");
+		countries.add("Tanzania");
+		countries.add("Malawi");
+		countries.add("Zambia");
+		countries.add("Eswatini");
+		countries.add("Djibouti");
+		countries.add("Lesotho");
+		countries.add("Botswana");
+		countries.add("Namibia");
+		countries.add("Central African Republic");
+		countries.add("Republic of the Congo");
+		countries.add("Equatorial Guinea");
+		countries.add("Democratic Republic of the Congo");
+		countries.add("Cameroon");
+		countries.add("Rwanda");
+		countries.add("Burundi");
+		countries.add("Chad");
+		countries.add("Niger");
+		countries.add("Mali");
+		countries.add("Mauritania");
+		countries.add("Guinea");
+		countries.add("Nigeria");
+		countries.add("Togo");
+		countries.add("South Sudan");
+		countries.add("Guinea-Bissau");
+		countries.add("Sierra Leone");
+		countries.add("Côte D'Ivore");
 	}
 	
 	public void keyPressed ()
@@ -18,8 +71,11 @@ public class QuizMode extends Map{
 		
 	}
 
-	public void draw (PApplet draw) 
+	public void draw(PApplet draw) 
 	{
+		draw.background(255, 255, 102);
 		
+		draw.textSize(30);
+		draw.text("MAIN MENU", draw.width/2, draw.height/2 - 170);
 	}
 }
