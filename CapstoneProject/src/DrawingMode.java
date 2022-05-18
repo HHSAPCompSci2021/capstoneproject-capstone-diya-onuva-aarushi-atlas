591125a7bb90a4bc09fb162fa780d7eee57b73ea
aarushiagupta <agupta740@student.fuhsd.org>
2022-05-18 12:26:39 -0700

    removed unnecessary code

--- CapstoneProject/src/DrawingMode.java
+++ CapstoneProject/src/DrawingMode.java
@@ -1,21 +1,5 @@
 import java.awt.Color;
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-
-import javax.sound.sampled.Line;
-import javax.swing.JComponent;
-
-import processing.core.PApplet;
 
 /**
  * Serves to act as the draw feature for the class which is an interactive tool allowing the user to draw on a white board with four color options
@@ -25,12 +9,12 @@ import processing.core.PApplet;
  */
 public class DrawingMode extends Screen {
 
-	private Graphics2D g2;
 	private static boolean yes;
 	private static boolean isDragged;
 	private static boolean setBackground;
 	Color color;
 	private DrawingSurface surface;
+	private Menu menu;
 	
 	public DrawingMode(DrawingSurface draw) {
 
@@ -58,31 +42,13 @@ public class DrawingMode extends Screen {
 			surface.stroke(10, 30, 40);
 			surface.line(surface.pmouseX, surface.pmouseY, surface.mouseX, surface.mouseY);
 		}
-//		surface.stroke(10, 30, 40);
-//		surface.line(surface.pmouseX, surface.pmouseY, surface.mouseX, surface.mouseY);
 		
-//		if (draw.mousePressed) {
-//			clicked = true;
-//			draw.fill(10, 15, 14);
-//			
-//			draw.rect(draw.mouseX, draw.mouseY, 5, 5);
-//			
-//		}
+		surface.rect(30, 30, 30, 30);
 		
-//		if(draw.mouseReleased() == true)
-		
-		
-//		if(clicked) {
-//			draw.fill(10, 15, 14);
-//		
-//			draw.line(draw.mouseX, draw.mouseY, draw.mouseX+5, draw.mouseY +10);
-//		}
-		
-//		if(clicked) {
-//			surface.fill(10, 15, 14);
-//			
-//			surface.rect(surface.pmouseX, surface.pmouseY, 20, 20);
-//		}
+		if(surface.mousePressed && surface.mouseX <= 60 && surface.mouseX >= 30 && surface.mouseY <= 60 && surface.mouseY >= 30) {
+			setBackground = true;
+			surface.switchScreen(ScreenSwitcher.MENU_SCREEN);
+		}
 	}
 	
 	/**
@@ -97,9 +63,16 @@ public class DrawingMode extends Screen {
 //	}
 	
 	public void mouseClicked() {
-		surface.fill(10, 15, 14);
 		
-		surface.rect(surface.pmouseX, surface.pmouseY, 20, 20);
+		if (surface.mouseX <= 60 && surface.mouseX >= 30 && surface.mouseY <= 60 && surface.mouseY >= 30) {
+//			surface.background(255);
+			setBackground = true;
+		}
+		else {
+			surface.fill(10, 15, 14);
+			surface.rect(surface.pmouseX, surface.pmouseY, 20, 20);
+		}
+
 	}
 	
 	public void mouseDragged() {
@@ -117,30 +90,28 @@ public class DrawingMode extends Screen {
 	 * sets the paint color to red
 	 */
 	public void red() {
-		g2.setPaint(Color.red);
+		surface.fill(DRAWING_HEIGHT);
 	}
 	
 	/**
 	 * sets the paint color to black
 	 */
 	public void black() {
-		g2.setPaint(Color.black);
+		surface.fill(DRAWING_HEIGHT);
 	}
 	
 	/**
 	 * sets the paint color to green
 	 */
 	public void green() {
-		g2.setPaint(Color.green);
+		surface.fill(DRAWING_HEIGHT);
 	}
 	
 	/**
 	 * sets the paint color to blue
 	 */
 	public void blue() {
-		g2.setPaint(Color.blue);
+		surface.fill(DRAWING_HEIGHT);
 	}
 
-
-	
 }
