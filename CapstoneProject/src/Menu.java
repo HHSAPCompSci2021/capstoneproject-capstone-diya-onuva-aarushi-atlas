925fca6f3838a3ccaf2da3c5a1a5b3f9e6b4ddc3
Onuva Ekram <oekram574@student.fuhsd.org>
2022-05-18 19:21:53 -0700

    Update Menu.java

--- CapstoneProject/src/Menu.java
+++ CapstoneProject/src/Menu.java
@@ -7,8 +7,7 @@ import processing.core.PImage;
 
 /**
  * 
- * Has a StudyMode, QuizMode, highscore, Instructions, PFont, if pressed, int for mode
- * Serves to act as a menu where the user can navigate to different features within the game (study, quiz, and draw mode as well as the instructions)
+ * Serves to act as a menu where the user can navigate to different <code>Screens</code> within the game (<code>StudyMode</code>, <code>QuizMode</code>, <code>DrawMode</code>, and the <code>Instructions</code>)
  *
  * @author Aarushi Gupta
  * 
@@ -16,52 +15,38 @@ import processing.core.PImage;
 
 public class Menu extends Screen {
 
-	private Instructions inst;
-	private QuizMode quiz;
-	private DrawingMode drawing;
-	private StudyMode study;
-	private DrawingSurface draw;
-	private int mode;
-	private Rectangle studyBtn;
-	private Rectangle quizBtn;
-	private Rectangle drawingBtn;
-	private Rectangle instructBtn;
-	
-	private boolean pressed;
-	private float bW, bH, normT, factor;
-	
 	private PImage bg;
+	private DrawingSurface draw;
+	private float bW, bH, normT, factor, x, y;
 	
+	/**
+	 * Sets default settings
+	 * @param draw given PApplet
+	 */
 	public Menu(DrawingSurface draw)
 	{
 		super(1200, 750);
 		this.draw = draw;
 		
-		inst = new Instructions(draw);
-		quiz = new QuizMode(draw);
-		study = new StudyMode(draw);
-		drawing = new DrawingMode(draw);
-		
-		studyBtn = new Rectangle(1200/2, 750/2 - 150, 350, 50);
-		quizBtn = new Rectangle (1200/2, 750/2, 350, 50);
-		drawingBtn = new Rectangle (1200/2 , 750/2 + 150, 350, 50);
-		instructBtn = new Rectangle (1200/2 , 750/2 + 300, 350, 50);
-		
-		pressed = false;
 		factor = 0.9f;
 		bW = DRAWING_WIDTH * 0.23f;
 		bH = DRAWING_HEIGHT * 0.09f;
 		normT = 35;
 		
+		x = -1;
+		y = -1;
+		
 	}
 	
+	/**
+	 * Completes PApplet setup
+	 */
 	public void setup() {
 		bg = draw.loadImage("fileData/MenuMap.jpg");
 	}
 	
 	/**
-	 * Draws the chosen Mode
-	 * @param draw
+	 * Draws the <code>Menu</code> <code>Screen</code>
 	 */
 	public void draw() 
 	{
@@ -71,8 +56,16 @@ public class Menu extends Screen {
 		draw.rectMode(PConstants.CENTER);
 		draw.noStroke();
 		
-		if (draw.mousePressed && draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
-				&& draw.mouseY <= draw.height*0.43f + bH/2 && draw.mouseY >= draw.height*0.43f - bH/2) {
+		if (draw.mousePressed && x < 0 && y < 0) {
+			x = draw.mouseX;
+			y = draw.mouseY;
+		}else if (!draw.mousePressed) {
+			x = -1;
+			y = -1;
+		}
+		
+		if (draw.mousePressed && x <= draw.width/2 + bW/2 && x >= draw.width/2 - bW/2
+				&& y <= draw.height*0.43f + bH/2 && y >= draw.height*0.43f - bH/2) {
 			draw.fill(25, 210, 165);
 			draw.rect(draw.width/2, draw.height * 0.43f, bW * factor, bH * factor, 5);
 			draw.fill(0);
@@ -87,8 +80,8 @@ public class Menu extends Screen {
 			draw.text("Study Mode", draw.width/2, draw.height * 0.43f + normT/3);
 		}
 		
-		if (draw.mousePressed && draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
-				&& draw.mouseY <= draw.height*0.6f + bH/2 && draw.mouseY >= draw.height*0.6f - bH/2) {
+		if (draw.mousePressed && x <= draw.width/2 + bW/2 && x >= draw.width/2 - bW/2
+				&& y <= draw.height*0.6f + bH/2 && y >= draw.height*0.6f - bH/2) {
 			draw.fill(25, 210, 165);
 			draw.rect(draw.width/2, draw.height * 0.6f, bW * factor, bH * factor, 5);
 			draw.fill(0);
@@ -103,8 +96,8 @@ public class Menu extends Screen {
 			draw.text("Quiz Mode", draw.width/2, draw.height * 0.6f + normT/3);
 		}
 		
-		if (draw.mousePressed && draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
-				&& draw.mouseY <= draw.height*0.77f + bH/2 && draw.mouseY >= draw.height*0.77f - bH/2) {
+		if (draw.mousePressed && x <= draw.width/2 + bW/2 && x >= draw.width/2 - bW/2
+				&& y <= draw.height*0.77f + bH/2 && y >= draw.height*0.77f - bH/2) {
 			draw.fill(25, 210, 165);
 			draw.rect(draw.width/2, draw.height * 0.77f, bW * factor, bH * factor, 5);
 			draw.fill(0);
@@ -119,8 +112,8 @@ public class Menu extends Screen {
 			draw.text("Draw Mode", draw.width/2, draw.height * 0.77f + normT/3);
 		}
 		
-		if (draw.mousePressed && draw.mouseX <= draw.width * 0.08f + bH*0.85f/2 && draw.mouseX >= draw.width * 0.08f - bH*0.85f/2
-				&& draw.mouseY <= draw.height * 0.09f + bH*0.85f/2 && draw.mouseY >= draw.height * 0.09f - bH*0.85f/2) {
+		if (draw.mousePressed && x <= draw.width * 0.08f + bH*0.85f/2 && x >= draw.width * 0.08f - bH*0.85f/2
+				&& y <= draw.height * 0.09f + bH*0.85f/2 && y >= draw.height * 0.09f - bH*0.85f/2) {
 			draw.fill(120, 130, 140);
 			draw.ellipse(draw.width * 0.08f, draw.height * 0.09f, bH*0.85f * factor, bH*0.85f * factor);
 			draw.fill(0);
@@ -142,39 +135,37 @@ public class Menu extends Screen {
 		
 	}
 	
-	
-	public void mouseClicked() {
-		
-		draw.textAlign(PConstants.CENTER);
-		draw.rectMode(PConstants.CENTER);
-		
-		if (draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
-				&& draw.mouseY <= draw.height*0.43f + bH/2 && draw.mouseY >= draw.height*0.43f - bH/2) 
-			draw.switchScreen(ScreenSwitcher.STUDY_SCREEN);
-		else if (draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
-				&& draw.mouseY <= draw.height*0.6f + bH/2 && draw.mouseY >= draw.height*0.6f - bH/2)
-			draw.switchScreen(ScreenSwitcher.QUIZ_SCREEN);
-		else if (draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
-				&& draw.mouseY <= draw.height*0.77f + bH/2 && draw.mouseY >= draw.height*0.77f - bH/2) 
-			draw.switchScreen(ScreenSwitcher.DRAW_SCREEN);
-		else if (draw.mouseX <= draw.width * 0.08f + bH*0.85f/2 && draw.mouseX >= draw.width * 0.08f - bH*0.85f/2
-				&& draw.mouseY <= draw.height * 0.09f + bH*0.85f/2 && draw.mouseY >= draw.height * 0.09f - bH*0.85f/2) 
-			draw.switchScreen(ScreenSwitcher.INSTRUCTIONS_SCREEN);
-	}
-	
 	/**
-	 * To say which mode the user is currently in
-	 * @return a string with what mode is currently selected
+	 * Detects when mouse is released
 	 */
-	public String getMode()
-	{
-		if (mode == 1)
-			return "Study Mode";
-		if (mode == 2)
-			return "Quiz Mode";
-		if (mode == 3)
-			return "Draw Mode";
-		else
-			return "No mode selected";
+	public void mouseReleased() {
+		if (draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
+				&& draw.mouseY <= draw.height*0.43f + bH/2 && draw.mouseY >= draw.height*0.43f - bH/2
+				&& x <= draw.width/2 + bW/2 && x >= draw.width/2 - bW/2
+				&& y <= draw.height*0.43f + bH/2 && y >= draw.height*0.43f - bH/2) {
+			draw();
+			draw.switchScreen(ScreenSwitcher.STUDY_SCREEN);
+		}
+		else if (draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
+				&& draw.mouseY <= draw.height*0.6f + bH/2 && draw.mouseY >= draw.height*0.6f - bH/2
+				&& x <= draw.width/2 + bW/2 && x >= draw.width/2 - bW/2
+				&& y <= draw.height*0.6f + bH/2 && y >= draw.height*0.6f - bH/2) {
+			draw();
+			draw.switchScreen(ScreenSwitcher.QUIZ_SCREEN);
+		}
+		else if (draw.mouseX <= draw.width/2 + bW/2 && draw.mouseX >= draw.width/2 - bW/2
+				&& draw.mouseY <= draw.height*0.77f + bH/2 && draw.mouseY >= draw.height*0.77f - bH/2
+				&& x <= draw.width/2 + bW/2 && x >= draw.width/2 - bW/2
+				&& y <= draw.height*0.77f + bH/2 && y >= draw.height*0.77f - bH/2) {
+			draw();
+			draw.switchScreen(ScreenSwitcher.DRAW_SCREEN);
+		}
+		else if (draw.mouseX <= draw.width * 0.08f + bH*0.85f/2 && draw.mouseX >= draw.width * 0.08f - bH*0.85f/2
+				&& draw.mouseY <= draw.height * 0.09f + bH*0.85f/2 && draw.mouseY >= draw.height * 0.09f - bH*0.85f/2
+				&& x <= draw.width * 0.08f + bH*0.85f/2 && x >= draw.width * 0.08f - bH*0.85f/2
+				&& y <= draw.height * 0.09f + bH*0.85f/2 && y >= draw.height * 0.09f - bH*0.85f/2) {
+			draw();
+			draw.switchScreen(ScreenSwitcher.INSTRUCTIONS_SCREEN);
+		}
 	}
 }
