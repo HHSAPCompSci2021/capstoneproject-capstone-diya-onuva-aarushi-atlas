e9d705640b81224505e88a3086bd627093c65e5c
Onuva Ekram <oekram574@student.fuhsd.org>
2022-05-22 00:40:35 -0700

    Added sound

--- CapstoneProject/src/DrawingSurface.java
+++ CapstoneProject/src/DrawingSurface.java
@@ -1,6 +1,11 @@
 import java.awt.Point;
-
+import java.io.File;
 import java.util.ArrayList;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 
 import processing.core.PApplet;
 import processing.core.PFont;
@@ -15,13 +20,15 @@ import processing.core.PImage;
 public class DrawingSurface extends PApplet implements ScreenSwitcher {
 	
 	private float ratioX, ratioY;
-	
+	private static Music m;
+	private static int frame;
 	private static Introduction intro;
 	private static Menu menu;
 	private static StudyMode study;
 	private static QuizMode quiz;
 	private static DrawingMode drawing;
 	private static Instructions instruct;
+	private static String track;
 	
 	private Screen activeScreen;
 	private ArrayList<Screen> screens;
@@ -53,7 +60,12 @@ public class DrawingSurface extends PApplet implements ScreenSwitcher {
 		screens.add(instruct);
 		
 		activeScreen = screens.get(0);
-		
+		frame = 0;
+		m = new Music();
+		track = "fileData/5 Minute Timer - Calm and Relaxing Music.wav"; //edit
+
+		m.setFile(track);
+		m.play();
 	}
 	
 	/**
@@ -150,6 +162,69 @@ public class DrawingSurface extends PApplet implements ScreenSwitcher {
 	 */
 	public void switchScreen(int i) {
 		activeScreen = screens.get(i);
+	}
+	
+	public static class Music {
+		static Clip clip;
+		static float vol = -10;
+		boolean started = false;
+
+		public void setFile(String soundFileName) {
+			try {
+				File file = new File(soundFileName);
+				AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+				clip = AudioSystem.getClip();
+				clip.open(sound);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+
+			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			gainControl.setValue(vol);
+		}
+
+		public void setVol(float v) {
+			if (vol + v <= 6.0206 && vol + v >= -80) {
+				vol += v;
+				FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+				gainControl.setValue(vol);
+			}
+			System.out.println("Volume is at " + (int)((vol+80)/86.0206 * 100) + "%");
+		}
+		public void restart() {
+			frame = 0;
+			play();
+		}
+
+		public void play() {
+			clip.setFramePosition(frame);
+			clip.start();
+			started = true;
+		}
+		
+//		public float progress() {
+//			if(clip.getFramePosition() <= 0 && clip.getFrameLength() !=0)  return (float)frame/clip.getFrameLength();
+//			else if(clip.getFrameLength() !=0) return (float)clip.getFramePosition()/clip.getFrameLength();
+//			else return 1;
+//		}
+		
+//		public void scrolling(double num) {
+//			frame = (int) (clip.getFrameLength() * num);
+//			clip.stop();
+//			clip.close();
+//			started = false;
+//		}
+		
+		public void loop() {
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		}
+
+		public void stop() {
+			frame = clip.getFramePosition();
+			clip.stop();
+			clip.close();
+		}
+
 	}
 
 }
